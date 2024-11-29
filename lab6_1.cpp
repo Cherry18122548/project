5680229f101e39ed0b92b01e0297e3a930072516
Cherry18122548 <Cherryworanat@gmail.com>
2024-11-29 17:09:52 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int N ;
+    int even =0,Odd =0;
+    while(true){
+        cout << "Enter an integer: ";
+        cin >> N;
+        if(N==0){
+            break;
+            
+        }
+        if(N%2==0){
+            even++;
+        }else{
+            Odd++;
+        }  
+    }
+    cout << "#Even numbers = " << even << "\n";
+    cout << "#Odd numbers = " << Odd ;
     return 0;
 }
